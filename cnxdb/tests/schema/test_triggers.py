5b555c0faa804fee4cace868f1f43d958dc204d2
Michael Mulich <michael.mulich@gmail.com>
2017-05-04 17:34:16 -0700

    Only run the trigger tests within the python2.7 context
    This is because the the venv.activate_venv schema function is not
    python3 compatible and isn't going to be without some changes. Furthermore,
    all the existing triggers are defined to run under python2 (plpythonu).

--- cnxdb/tests/schema/test_triggers.py
+++ cnxdb/tests/schema/test_triggers.py
@@ -2,9 +2,21 @@
 import json
 import uuid
 
+import pytest
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
+from .. import testing
 
+
+# Note, the triggers are only python 2.x compatible. It's assumed,
+# at least for now, that in-database logic (i.e. triggers) are only
+# run within a python2 environment. This product is to be setup in a
+# production environment running within the database under python2 and
+# optionally running within application code under either python2 or python3.
+
+
+@pytest.mark.skipif(testing.is_py3(),
+                    reason="triggers are only python2.x compat")
 class TestPostPublication:
 
     channel = 'post_publication'
