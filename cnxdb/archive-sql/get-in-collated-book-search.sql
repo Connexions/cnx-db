2ad840616878fc71b653cada79b7e760865e2f99
Ross Reedstrom <reedstrm@gmail.com>
2017-05-23 17:19:05 -0500

    speed up in book search for collated content

--- cnxdb/archive-sql/get-in-collated-book-search.sql
+++ cnxdb/archive-sql/get-in-collated-book-search.sql
@@ -6,8 +6,8 @@
 -- ###
 
 -- arguments: uuid:string, version:string, search_term:string
-WITH RECURSIVE t(node, title, path,value, depth, corder) AS (
-SELECT nodeid, title, ARRAY[nodeid], documentid, 1, ARRAY[childorder]
+WITH RECURSIVE t(node, title, path, docs, value, depth, corder) AS (
+SELECT nodeid, title, ARRAY[nodeid], ARRAY[documentid], documentid, 1, ARRAY[childorder]
 FROM
   trees tr,
   modules m
@@ -18,7 +18,7 @@ WHERE
   tr.parent_id IS NULL AND
   is_collated = True
 UNION ALL
-SELECT c1.nodeid, c1.title, t.path || ARRAY[c1.nodeid], c1.documentid, t.depth+1, t.corder || ARRAY[c1.childorder]
+SELECT c1.nodeid, c1.title, t.path || ARRAY[c1.nodeid], t.docs || ARRAY[c1.documentid], c1.documentid, t.depth+1, t.corder || ARRAY[c1.childorder]
 FROM trees c1 JOIN t ON (c1.parent_id = t.node)
 WHERE NOT nodeid = any (t.path)
 )
@@ -36,12 +36,12 @@ E'StartSel="<span class=""q-match"">", StopSel="</span>", MaxFragments=1, MaxWor
 count_collated_lexemes(cft.item, cft.context, %(search_term)s) as matches,
 ts_rank_cd(cft.module_idx, plainto_tsquery(%(search_term)s)) AS rank
 FROM
- t as tt join
- collated_fti cft on (cft.context = tt.value and tt.depth = 1) join
+ collated_fti cft join
  modules m on cft.item = m.module_ident join
  t on t.value = m.module_ident
 
 WHERE
+ cft.context = t.docs[1] AND
  cft.module_idx @@ plainto_tsquery(%(search_term)s)
 ORDER BY
  rank DESC,
