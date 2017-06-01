48a0ec8eabb39492f022db8e90646311b494fae1
Ross Reedstrom <reedstrm@gmail.com>
2017-06-01 09:28:03 -0500

    make migration conditional

--- cnxdb/migrations/20170530143035_search-modtags-index.py
+++ cnxdb/migrations/20170530143035_search-modtags-index.py
@@ -2,8 +2,15 @@
 
 
 def up(cursor):
-    cursor.execute('CREATE INDEX moduletags_module_ident_idx '
-                   'on moduletags (module_ident)')
+    cursor.execute("SELECT 1 FROM pg_indexes "
+                   "WHERE tablename  = 'moduletags' "
+                   "AND indexname = 'moduletags_module_ident_idx'")
+    if cursor.rowcount == 0:
+        # FUTURE postgresql > 9.4 has "IF NOT EXISTS" for this case
+        cursor.execute('CREATE INDEX moduletags_module_ident_idx '
+                       'on moduletags (module_ident)')
+
 
 def down(cursor):
+    # FUTURE postgresql > 9.4 has "IF EXISTS" for this case
     cursor.execute('DROP INDEX moduletags_module_ident_idx')
