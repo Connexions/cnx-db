25bc343d599115efcf979064cd7f24a9bc2d4232
Michael Mulich <michael.mulich@gmail.com>
2017-06-15 23:17:40 -0700

    Add association table for celery results to documents

--- cnxdb/publishing-sql/schema-tables.sql
+++ cnxdb/publishing-sql/schema-tables.sql
@@ -111,10 +111,13 @@ CREATE TABLE api_keys (
 );
 
 
-CREATE TABLE post_publications (
+CREATE TABLE document_baking_result_associations (
+  -- This associates the modules.module_ident with the celery result id.
   "module_ident" INTEGER NOT NULL,
-  "state" post_publication_states NOT NULL,
-  "state_message" TEXT,
-  "timestamp" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  -- We loosely associate the result_id, because it could be in the database
+  -- or in some other storage service depending on the configuration.
+  "result_id" UUID NOT NULL,
+  "created" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY ("module_ident", "result_id"),
   FOREIGN KEY ("module_ident") REFERENCES modules ("module_ident")
 );
