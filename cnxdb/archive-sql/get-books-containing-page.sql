de909490220524a6d05b30d6611525f2bedf2aaf
Cade Ritter <ritter.cade@gmail.com>
2017-07-28 10:31:45 -0500

    make sql unaware

--- cnxdb/archive-sql/get-books-containing-page.sql
+++ cnxdb/archive-sql/get-books-containing-page.sql
@@ -5,12 +5,12 @@
 -- See LICENCE.txt for details.
 -- ###
 
--- arguments: page_uuid:string; page_version:string
+-- arguments: document_uuid:string; document_version:string
 WITH RECURSIVE t(node, title, parent, path, value) AS (
   SELECT nodeid, title, parent_id, ARRAY[nodeid], documentid
   FROM trees tr, modules m
-  WHERE m.uuid = %(page_uuid)s::uuid
-  AND module_version(m.major_version, m.minor_version) = %(page_version)s
+  WHERE m.uuid = %(document_uuid)s::uuid
+  AND module_version(m.major_version, m.minor_version) = %(document_version)s
   AND tr.documentid = m.module_ident
   AND tr.parent_id IS NOT NULL
 UNION ALL
