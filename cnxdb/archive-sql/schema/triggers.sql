20c17972d10453d49fdc874557db126530898dab
Ross Reedstrom <reedstrm@gmail.com>
2017-08-09 15:53:47 -0500

    change update_latest trigger to do expected behavior when multiple minor versions are in flight (#43)

--- cnxdb/archive-sql/schema/triggers.sql
+++ cnxdb/archive-sql/schema/triggers.sql
@@ -1,8 +1,8 @@
 CREATE OR REPLACE FUNCTION update_latest() RETURNS trigger AS '
 BEGIN
   IF (TG_OP = ''INSERT'' OR TG_OP = ''UPDATE'') AND
-          NEW.revised >= ((SELECT revised FROM modules
-              WHERE uuid = NEW.uuid ORDER BY revised DESC LIMIT 1)
+          NEW.revised >= ((SELECT revised FROM latest_modules
+              WHERE uuid = NEW.uuid  ORDER BY revised DESC LIMIT 1)
               UNION ALL VALUES (NEW.revised) LIMIT 1) AND
           NEW.stateid = 1 THEN
       DELETE FROM latest_modules WHERE moduleid = NEW.moduleid OR uuid = NEW.uuid;
