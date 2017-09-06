a7ac52fb9ae7e8c22f5fca32a59d296c7ee308a4
Ross Reedstrom <reedstrm@gmail.com>
2017-09-06 14:42:58 -0500

    WIP use better list of steps to migrate

--- cnxdb/migrations/20170816204849_subcol-uuid-data.py
+++ cnxdb/migrations/20170816204849_subcol-uuid-data.py
@@ -5,25 +5,21 @@ from datetime import timedelta
 
 from dbmigrator import deferred
 
-logger = logging.getLogger('migration')
+logger = logging.getLogger('dbmigrator')
 
 
 def _batcher(seq, size):
     return (seq[pos:pos + size] for pos in range(0, len(seq), size))
 
 
+@deferred
 def up(cursor):
     """Add SubCollection uuids and metadata records for all latest books"""
 
-    cursor.execute("""SELECT DISTINCT ident_hash(uuid, major_version, minor_version)
-            FROM trees t
-              JOIN trees p ON t.parent_id = p.nodeid
-              JOIN latest_modules m ON p.documentid=m.module_ident
-            WHERE t.documentid is null""")
-    anon_subcols = [r[0].split('@') for r in cursor.fetchall()]
+    anon_subcols = should_run(cursor)
 
     num_todo = len(anon_subcols)
-    logger.debug('Books to add chapter uuids: {}'.format(num_todo))
+    logger.info('Books to add chapter uuids: {}'.format(num_todo))
 
     if num_todo > 10:
         batch_size = int(num_todo / 10)
@@ -44,23 +40,43 @@ def up(cursor):
         elapsed = time.time() - start
         remaining_est = elapsed * (num_todo - num_complete) / num_complete
         est_complete = start + remaining_est
-        logger.debug('{}% complete; '
-                     'est: "{}" ({})'.format(percent_comp,
-                                             time.ctime(est_complete),
-                                             timedelta(0, remaining_est)))
+        logger.info('{}% complete; '
+                    'est: "{}" ({})'.format(percent_comp,
+                                            time.ctime(est_complete),
+                                            timedelta(0, remaining_est)))
 
 
 def down(cursor):
     cursor.execute("DELETE FROM modules WHERE portal_type = 'SubCollection'")
-    cursor.execute("""UPDATE trees t SET documentid = NULL WHERE NOT EXISTS
-            (SELECT 1 FROM modules m WHERE m.module_ident = t.documentid""")
+    cursor.execute("""UPDATE trees t SET documentid = NULL
+                      WHERE documentid is not NULL
+                      AND NOT EXISTS (SELECT 1 FROM modules m
+                            WHERE m.module_ident = t.documentid""")
 
 
 def should_run(cursor):
     """Check if any latest books still have chapters without uuids."""
-    cursor.execute("""SELECT ident_hash(uuid,major_version,minor_version) FROM trees t
-              JOIN trees p ON t.parent_id = p.nodeid
-              JOIN latest_modules m ON p.documentid=m.module_ident
-            WHERE t.documentid IS NULL""")
-    anon_subcols = cursor.fetchone()[0]
+    cursor.execute("""WITH RECURSIVE t(node, title, path, documentid, parent,
+     depth, corder, is_collated) AS (
+    SELECT nodeid, title, ARRAY[nodeid], documentid, NULL::integer, 1,
+           ARRAY[childorder], is_collated
+    FROM trees tr, latest_modules m
+    WHERE
+      tr.documentid = m.module_ident AND
+      tr.parent_id IS NULL AND
+      tr.is_collated = False
+UNION ALL
+    SELECT c1.nodeid, c1.title, t.path || ARRAY[c1.nodeid],
+    c1.documentid, t.documentid, t.depth+1, t.corder || ARRAY[c1.childorder],
+    c1.is_collated
+    FROM trees c1 JOIN t ON (c1.parent_id = t.node)
+    WHERE not nodeid = any (t.path) AND t.is_collated = c1.is_collated
+)
+
+SELECT DISTINCT ident_hash(m.uuid, m.major_version, m.minor_version), t.depth
+           FROM t JOIN trees b ON t.path[1] = b.nodeid JOIN
+           latest_modules m ON b.documentid=m.module_ident
+           WHERE t.documentid is null
+        ORDER by 1,2""")
+    anon_subcols = [r[0].split('@') for r in cursor.fetchall()]
     return anon_subcols
