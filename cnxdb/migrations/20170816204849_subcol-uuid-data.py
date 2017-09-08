787f572f92da814510c9afb559ff2ad5bc5df5d2
Ross Reedstrom <reedstrm@gmail.com>
2017-09-08 12:01:00 -0500

    detect and skip books w/ duplicate chapter names

--- cnxdb/migrations/20170816204849_subcol-uuid-data.py
+++ cnxdb/migrations/20170816204849_subcol-uuid-data.py
@@ -18,6 +18,16 @@ def up(cursor):
 
     anon_subcols = should_run(cursor)
 
+    dups = _dup_titles(cursor)
+    if dups:
+        logger.info('Skipping books w/ duplicate chapter titles')
+        for dup_id, dup_title in dups:
+            logger.info('    {} {}'.format(dup_id, dup_title))
+            item = dup_id.split('@')
+            # Some books are included more than once
+            while item in anon_subcols:
+                anon_subcols.remove(item)
+
     num_todo = len(anon_subcols)
     logger.info('Books to add chapter uuids: {}'.format(num_todo))
 
@@ -26,24 +36,32 @@ def up(cursor):
     else:
         batch_size = num_todo
 
-    if batch_size > 100:
-        batch_size = 100
+    if batch_size > 10:
+        batch_size = 10
 
+    logger.info('Batch size: {}'.format(batch_size))
     start = time.time()
+    guesstimate = 7 * num_todo
+    guess_complete = guesstimate + start
+    logger.info('Completion guess: '
+                '"{}" ({})'.format(time.ctime(guess_complete),
+                                   timedelta(0, guesstimate)))
     num_complete = 0
     for batch in _batcher(anon_subcols, batch_size):
         for book in batch:
             cursor.execute("SELECT subcol_uuids(%s, %s)", book)
         cursor.connection.commit()
         num_complete += len(batch)
-        percent_comp = (num_complete * 100) / num_todo
+        percent_comp = ((num_complete * 1000) / num_todo) / 10.0
         elapsed = time.time() - start
         remaining_est = elapsed * (num_todo - num_complete) / num_complete
-        est_complete = start + remaining_est
-        logger.info('{}% complete; '
+        est_complete = start + elapsed + remaining_est
+        logger.info('{}% complete '
                     'est: "{}" ({})'.format(percent_comp,
                                             time.ctime(est_complete),
                                             timedelta(0, remaining_est)))
+
+    logger.info('Total runtime: {}'.format(timedelta(0, elapsed)))
 
 
 def down(cursor):
@@ -80,3 +98,31 @@ SELECT DISTINCT ident_hash(m.uuid, m.major_version, m.minor_version), t.depth
         ORDER by 1,2""")
     anon_subcols = [r[0].split('@') for r in cursor.fetchall()]
     return anon_subcols
+
+
+def _dup_titles(cursor):
+    """Return duplicate chapter title info"""
+    cursor.execute("""WITH RECURSIVE t(node, title, path, documentid, parent,
+    depth, corder, is_collated) AS (
+    SELECT nodeid, title, ARRAY[nodeid], documentid, NULL::integer, 1,
+    ARRAY[childorder],
+           is_collated
+    FROM trees tr, latest_modules m
+    WHERE
+      tr.documentid = m.module_ident AND
+      tr.parent_id IS NULL AND
+      tr.is_collated = False
+UNION ALL
+    SELECT c1.nodeid, c1.title, t.path || ARRAY[c1.nodeid], c1.documentid,
+    t.node, t.depth+1, t.corder || ARRAY[c1.childorder], c1.is_collated
+    FROM trees c1 JOIN t ON (c1.parent_id = t.node)
+    WHERE not nodeid = any (t.path) AND t.is_collated = c1.is_collated
+)
+
+SELECT ident_hash(m.uuid, m.major_version, m.minor_version), t.title
+           FROM t JOIN trees b ON t.path[1] = b.nodeid JOIN
+           latest_modules m ON b.documentid=m.module_ident
+           WHERE t.documentid is null
+        GROUP by ident_hash(m.uuid, m.major_version, m.minor_version),
+                 t.parent, t.title having count(*) > 1""")
+    return cursor.fetchall()
