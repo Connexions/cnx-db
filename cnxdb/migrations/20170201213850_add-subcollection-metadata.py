0c8c5eb5b9d2a4aeab64308d37fb0cdf0ad14e77
Karen Chan <karen@karen-chan.com>
2017-09-21 17:04:22 +0100

    Use postgres super user in migrations that require it (#71)
    * Create plpython function with super user in migration
    
    When running the migrations 20160723123620_add_sql_function_strip_html,
    20170201213850_add-subcollection-metadata,
    20170208213802_ident-hash-func against a database where
    the user is not a super user, we see this error:
    
    ```
    psycopg2.ProgrammingError: permission denied for language plpythonu
    ```
    
    * Create extension pg_trgm with super user in migration
    
    When running the migration 20160723124137
    add_modules_strip_html_name_index against a database where the user is
    not a super user, we see this error:
    
    ```
    psycopg2.ProgrammingError: permission denied to create extension "pg_trgm"
    ```

--- cnxdb/migrations/20170201213850_add-subcollection-metadata.py
+++ cnxdb/migrations/20170201213850_add-subcollection-metadata.py
@@ -3,6 +3,8 @@ import os
 
 from contextlib import contextmanager
 
+from dbmigrator import super_user
+
 
 @contextmanager
 def open_here(filepath, *args, **kwargs):
@@ -17,12 +19,13 @@ def open_here(filepath, *args, **kwargs):
 def up(cursor):
     """Add subcol metadata support to shredding and data upgrades"""
 
-    cursor.execute("""
+    with super_user() as super_cursor:
+        super_cursor.execute("""
 CREATE FUNCTION uuid5(namespace uuid, name text) RETURNS uuid
     LANGUAGE plpythonu
     AS $_$ import uuid; return uuid.uuid5(uuid.UUID(namespace), name) $_$;""")
 
-    cursor.execute("""
+        super_cursor.execute("""
 CREATE OR REPLACE FUNCTION public.is_baked(col_uuid uuid, col_ver text)
  RETURNS boolean
  LANGUAGE sql
@@ -34,19 +37,19 @@ SELECT bool_or(is_collated)
           module_version(major_version, minor_version) = col_ver
 $function$;""")
 
-    with open_here('../archive-sql/schema/subcol_uuids_func.sql', 'rb') as f:
-        cursor.execute(f.read())
+        with open_here('../archive-sql/schema/subcol_uuids_func.sql',
+                       'rb') as f:
+            super_cursor.execute(f.read())
 
-    with open_here('../archive-sql/schema/shred_collxml.sql', 'rb') as f:
-        cursor.execute(f.read())
+        with open_here('../archive-sql/schema/shred_collxml.sql', 'rb') as f:
+            super_cursor.execute(f.read())
 
 
 def down(cursor):
-    # TODO rollback code
+    with super_user() as super_cursor:
+        super_cursor.execute('drop function uuid5(uuid, text)')
+        super_cursor.execute('drop function is_baked(uuid, text)')
+        super_cursor.execute('drop function subcol_uuids(text, text)')
 
-    cursor.execute('drop function uuid5(uuid, text)')
-    cursor.execute('drop function is_baked(uuid, text)')
-    cursor.execute('drop function subcol_uuids(text, text)')
-
-    with open_here('shred_collxml_20170201213850_pre.sql', 'rb') as f:
-        cursor.execute(f.read())
+        with open_here('shred_collxml_20170201213850_pre.sql', 'rb') as f:
+            super_cursor.execute(f.read())
