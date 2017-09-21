57eb7f6b8a71647cbb2375ca6f5b99d55aa38614
karen chan <karen@karen-chan.com>
2017-09-21 04:51:49 -0700

    Create extension pg_trgm with super user in migration
    When running the migration 20160723124137
    add_modules_strip_html_name_index against a database where the user is
    not a super user, we see this error:
    
    ```
    psycopg2.ProgrammingError: permission denied to create extension "pg_trgm"
    ```

--- cnxdb/migrations/20160723124137_add_modules_strip_html_name_index.py
+++ cnxdb/migrations/20160723124137_add_modules_strip_html_name_index.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
+from dbmigrator import super_user
+
 
 def up(cursor):
-    cursor.execute("""\
+    with super_user() as super_cursor:
+        super_cursor.execute("""\
 CREATE EXTENSION pg_trgm;
 CREATE INDEX modules_strip_html_name_trgm_gin ON modules \
     USING gin(strip_html(name) gin_trgm_ops);
@@ -10,7 +13,8 @@ CREATE INDEX modules_strip_html_name_trgm_gin ON modules \
 
 
 def down(cursor):
-    cursor.execute("""\
+    with super_user() as super_cursor:
+        super_cursor.execute("""\
 DROP INDEX IF EXISTS modules_strip_html_name_trgm_gin;
 DROP EXTENSION IF EXISTS pg_trgm;
 """)
