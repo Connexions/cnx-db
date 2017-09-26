604d48278ef590b19e67fcf7690d182f2dc97673
Ross Reedstrom <reedstrm@gmail.com>
2017-09-25 22:35:46 -0500

    remove separate lexeme tables - rewrite count functions

--- cnxdb/archive-sql/schema/fulltext-indexing.sql
+++ cnxdb/archive-sql/schema/fulltext-indexing.sql
@@ -37,18 +37,22 @@ CREATE OR REPLACE FUNCTION xml_to_baretext(xml) RETURNS text AS $$
 </xsl:stylesheet>
 $$ LANGUAGE xslt;
 
-CREATE OR REPLACE FUNCTION count_lexemes (myident int, mysearch text) RETURNS bigint as $$
-     select sum(array_length(positions,1))
-            from modulefti_lexemes,
-                 regexp_split_to_table(strip(to_tsvector(mysearch))::text,' ') s
-            where module_ident = myident and lexeme = substr(s,2,length(s)-2)
-$$ LANGUAGE SQL STABLE;
+CREATE OR REPLACE FUNCTION count_lexemes(myident integer, mysearch text)
+ RETURNS bigint
+ LANGUAGE sql
+ STABLE
+AS $function$
+WITH lexemes AS (SELECT word, nentry FROM ts_stat('SELECT module_idx FROM modulefti WHERE module_ident = ' || myident)),
+     words AS (select regexp_split_to_table(mysearch,' ') AS qwords)
+    SELECT SUM(nentry)::bigint FROM lexemes, words WHERE word @@ plainto_tsquery(qwords);
+    $function$;
 
-CREATE OR REPLACE FUNCTION count_collated_lexemes (myident int, bookident int, mysearch text) RETURNS bigint as $$
-     select sum(array_length(positions,1))
-            from collated_fti_lexemes,
-                 regexp_split_to_table(strip(to_tsvector(mysearch))::text,' ') s
-            where item = myident and context = bookident and lexeme = substr(s,2,length(s)-2)
+
+CREATE OR REPLACE FUNCTION count_collated_lexemes (myident int, bookident int, mysearch text) RETURNS bigint
+AS $$
+WITH lexemes AS (SELECT word, nentry FROM ts_stat('SELECT module_idx FROM collated_fti WHERE item = ' || myident || ' and context = ' || bookident)),
+     words AS (select regexp_split_to_table(mysearch,' ') AS qwords)
+    SELECT SUM(nentry)::bigint FROM lexemes, words WHERE word @@ plainto_tsquery(qwords);
 $$ LANGUAGE SQL STABLE;
 
 CREATE OR REPLACE FUNCTION index_fulltext_trigger()
@@ -109,29 +113,6 @@ CREATE TRIGGER index_fulltext_upsert
     FOR EACH row
       EXECUTE PROCEDURE index_fulltext_upsert_trigger();
 
-CREATE OR REPLACE FUNCTION index_fulltext_lexeme_update_trigger()
-  RETURNS TRIGGER AS $$
-  BEGIN
-
-    DELETE from modulefti_lexemes where module_ident = NEW.module_ident;
-
-    INSERT into modulefti_lexemes (module_ident, lexeme, positions)
-       (with lex as (SELECT regexp_split_to_table(NEW.module_idx::text, E' \'') as t )
-       SELECT NEW.module_ident,
-              substring(t,1,strpos(t,E'\':')-1),
-              ('{'||substring(t,strpos(t,E'\':')+2)||'}')::int[] from lex) ;
-
-  RETURN NEW;
-  END;
-  $$
-  LANGUAGE plpgsql;
-
-DROP TRIGGER IF EXISTS index_fulltext_lexeme ON modulefti;
-CREATE TRIGGER index_fulltext_lexeme
-  BEFORE INSERT OR UPDATE ON modulefti
-    FOR EACH row
-      EXECUTE PROCEDURE index_fulltext_lexeme_update_trigger();
-
 CREATE OR REPLACE FUNCTION index_collated_fulltext_trigger()
   RETURNS TRIGGER AS $$
   DECLARE
@@ -160,26 +141,3 @@ CREATE TRIGGER index_collated_fulltext
   AFTER INSERT OR UPDATE ON collated_file_associations
     FOR EACH row 
       EXECUTE PROCEDURE index_collated_fulltext_trigger();
-
-CREATE OR REPLACE FUNCTION index_collated_fulltext_lexeme_update_trigger()
-  RETURNS TRIGGER AS $$
-  BEGIN
-
-    DELETE from collated_fti_lexemes where item = NEW.item AND context = NEW.context;
-
-    INSERT into collated_fti_lexemes (item, context, lexeme, positions)
-       (with lex as (SELECT regexp_split_to_table(NEW.module_idx::text, E' \'') as t )
-       SELECT NEW.item, NEW.context,
-              substring(t,1,strpos(t,E'\':')-1),
-              ('{'||substring(t,strpos(t,E'\':')+2)||'}')::int[] from lex) ;
-
-  RETURN NEW;
-  END;
-  $$
-  LANGUAGE plpgsql;
-
-DROP TRIGGER IF EXISTS index_collated_fulltext_lexeme ON collated_fti;
-CREATE TRIGGER index_collated_fulltext_lexeme
-  BEFORE INSERT OR UPDATE ON collated_fti
-    FOR EACH row
-      EXECUTE PROCEDURE index_collated_fulltext_lexeme_update_trigger();
