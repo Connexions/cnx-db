69e238da6c7aa3d810d4e8652aebaa13b48ed956
Ross Reedstrom <reedstrm@gmail.com>
2017-10-05 16:44:47 -0500

    get only the highest version for each book a page is in, return full ident_hash, as well as authors. Put same-as-page-authors first, since this is likely to be the orginal book the page was published in.  Returned as list of hashes in page content-extras

--- cnxdb/archive-sql/schema/functions.sql
+++ cnxdb/archive-sql/schema/functions.sql
@@ -202,3 +202,29 @@ CREATE OR REPLACE FUNCTION plainto_or_tsquery (TEXT) RETURNS tsquery AS $$
   SELECT to_tsquery( regexp_replace( $1, E'[\\s\'|:&()!]+','|','g') );
 $$ LANGUAGE SQL STRICT IMMUTABLE;
 
+-- Create a function that always returns the first non-NULL item
+CREATE OR REPLACE FUNCTION first_agg ( anyelement, anyelement )
+RETURNS anyelement LANGUAGE SQL IMMUTABLE STRICT AS $$
+        SELECT $1;
+$$;
+ 
+-- And then wrap an aggregate around it
+CREATE AGGREGATE FIRST (
+        sfunc    = first_agg,
+        basetype = anyelement,
+        stype    = anyelement
+);
+ 
+-- Create a function that always returns the last non-NULL item
+CREATE OR REPLACE FUNCTION last_agg ( anyelement, anyelement )
+RETURNS anyelement LANGUAGE SQL IMMUTABLE STRICT AS $$
+        SELECT $2;
+$$;
+ 
+-- And then wrap an aggregate around it
+CREATE AGGREGATE LAST (
+        sfunc    = last_agg,
+        basetype = anyelement,
+        stype    = anyelement
+);
+
