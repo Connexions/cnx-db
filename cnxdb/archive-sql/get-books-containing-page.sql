90310e3b17d1f282db4ab6a5a82d63ccf92a27f6
Ross Reedstrom <reedstrm@gmail.com>
2017-10-05 16:44:47 -0500

    get only the highest version for each book a page is in, return full ident_hash, as well as authors. Put same-as-page-authors first, since this is likely to be the orginal book the page was published in.  Returned as list of hashes in page content-extras

--- cnxdb/archive-sql/get-books-containing-page.sql
+++ cnxdb/archive-sql/get-books-containing-page.sql
@@ -14,12 +14,35 @@ WITH RECURSIVE t(node, title, parent, path, value) AS (
   AND tr.documentid = m.module_ident
   AND tr.parent_id IS NOT NULL
 UNION ALL
-  SELECT c1.nodeid, c1.title, c1.parent_id, t.path || ARRAY[c1.nodeid], c1.documentid /* Recursion */
+  SELECT c1.nodeid, c1.title, c1.parent_id,
+         t.path || ARRAY[c1.nodeid], c1.documentid
   FROM trees c1
   JOIN t ON (c1.nodeid = t.parent)
   WHERE not nodeid = any (t.path)
+),
+
+books(uuid, major_version, minor_version, title) AS (
+  SELECT m.uuid, m.major_version, m.minor_version, COALESCE(t.title, m.name),
+         m.authors
+  FROM t
+  JOIN modules m ON t.value = m.module_ident
+  WHERE t.parent IS NULL
+  ORDER BY uuid, major_version desc, minor_version desc
+),
+
+page(authors) as (
+  SELECT authors FROM modules m
+  WHERE m.uuid = %(document_uuid)s::uuid
+  AND module_version(m.major_version, m.minor_version) = %(document_version)s
+),
+
+top_books(title, ident_hash, authors) AS (
+SELECT first(title),
+       ident_hash(uuid, first(major_version), first(minor_version)),
+       first(authors)
+  FROM books GROUP BY uuid
 )
-SELECT m.name, m.uuid
-FROM t
-JOIN modules m on t.value = m.module_ident
-WHERE t.parent IS NULL;
+
+SELECT tb.title, tb.ident_hash, tb.authors
+  FROM top_books tb, page p ORDER BY tb.authors = p.authors DESC
+
