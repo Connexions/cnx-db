c803c792020d5e47a83d05539059b830148cc4fc
Ross Reedstrom <reedstrm@gmail.com>
2017-10-11 17:56:17 -0500

    do not use timestamps to determine latest content (#75)

--- cnxdb/archive-sql/schema/views.sql
+++ cnxdb/archive-sql/schema/views.sql
@@ -13,8 +13,23 @@ CREATE VIEW all_modules as
 			buylink, major_version, minor_version, print_style
 	FROM latest_modules;
 
-CREATE VIEW current_modules AS
-       SELECT * FROM modules m
-	      WHERE module_ident =
-		    (SELECT max(module_ident) FROM modules
-			    WHERE m.moduleid = moduleid );
+CREATE OR REPLACE VIEW current_modules AS
+WITH latest_idents (module_ident) AS (
+          SELECT module_ident FROM modules m2 join modulestates ms
+                on m2.stateid = ms.stateid
+                WHERE m2.major_version = (
+                    SELECT max(major_version) FROM modules m3
+                        WHERE m2.uuid = m3.uuid
+                )
+                AND
+                (m2.minor_version IS NULL OR
+                 m2.minor_version = (
+                    SELECT max(minor_version) FROM modules m4
+                        WHERE m2.uuid = m4.uuid AND
+                        m2.major_version = m4.major_version
+                    )
+                )
+                AND ms.statename = 'current'
+        )
+SELECT m.* FROM latest_idents li JOIN modules m
+ON m.module_ident = li.module_ident;
