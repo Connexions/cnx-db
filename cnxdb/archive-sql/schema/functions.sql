2fa56cb3ae0ab10599c7aae28c6410d59f6d2ba0
Michael Mulich <michael.mulich@gmail.com>
2017-10-16 21:22:48 -0700

    Replace cnxarchive.transforms imports with cnxdb and add a migration

--- cnxdb/archive-sql/schema/functions.sql
+++ cnxdb/archive-sql/schema/functions.sql
@@ -89,7 +89,7 @@ CREATE OR REPLACE FUNCTION html_abstract(abstract text)
   RETURNS text
 AS $$
   plpy.warning('This function is deprecated, please use html_abstract(<module_ident>')
-  from cnxarchive.transforms import transform_abstract_to_html
+  from cnxdb.triggers.transforms import transform_abstract_to_html
   html_abstract, warning_messages = transform_abstract_to_html(abstract, None, plpy)
   if warning_messages:
     plpy.warning(warning_messages)
@@ -99,7 +99,7 @@ $$ LANGUAGE plpythonu;
 CREATE OR REPLACE FUNCTION html_abstract(module_ident int)
   RETURNS text
 AS $$
-  from cnxarchive.transforms import transform_abstract_to_html
+  from cnxdb.triggers.transforms import transform_abstract_to_html
   plan = plpy.prepare("SELECT abstract FROM modules NATURAL JOIN abstracts WHERE module_ident = $1", ('integer',))
   abstract = plpy.execute(plan, (module_ident,))[0]['abstract']
   html_abstract, warning_messages = transform_abstract_to_html(abstract, module_ident, plpy)
@@ -116,7 +116,7 @@ CREATE OR REPLACE FUNCTION html_content(cnxml text)
   RETURNS text
 AS $$
   plpy.warning('This function is deprecated, please use html_content(<module_ident>')
-  from cnxarchive.transforms import transform_module_content
+  from cnxdb.triggers.transforms import transform_module_content
   html_content, warning_messages = transform_module_content(cnxml, 'cnxml2html', plpy)
   if warning_messages:
     plpy.warning(warning_messages)
@@ -126,7 +126,7 @@ $$ LANGUAGE plpythonu;
 CREATE OR REPLACE FUNCTION html_content(module_ident int)
   RETURNS text
 AS $$
-  from cnxarchive.transforms import transform_module_content
+  from cnxdb.triggers.transforms import transform_module_content
   plan = plpy.prepare("SELECT convert_from(file, 'utf-8') FROM module_files AS mf NATURAL JOIN files AS f WHERE module_ident = $1 AND (filename = 'index.cnxml' OR filename = 'index.html.cnxml')", ('integer',))
   cnxml = plpy.execute(plan, (module_ident,))[0]['convert_from']
   content, warning_messages = transform_module_content(cnxml, 'cnxml2html', plpy, module_ident)
@@ -139,7 +139,7 @@ $$ LANGUAGE plpythonu;
 CREATE OR REPLACE FUNCTION cnxml_abstract(module_ident int)
   RETURNS text
 AS $$
-  from cnxarchive.transforms import transform_abstract_to_cnxml
+  from cnxdb.triggers.transforms import transform_abstract_to_cnxml
   plan = plpy.prepare("SELECT html FROM modules NATURAL JOIN abstracts WHERE module_ident = $1", ('integer',))
   abstract = plpy.execute(plan, (module_ident,))[0]['html']
   cnxml_abstract, warning_messages = transform_abstract_to_cnxml(abstract, module_ident, plpy)
@@ -151,7 +151,7 @@ $$ LANGUAGE plpythonu;
 CREATE OR REPLACE FUNCTION cnxml_content(module_ident int)
   RETURNS text
 AS $$
-  from cnxarchive.transforms import transform_module_content
+  from cnxdb.triggers.transforms import transform_module_content
   plan = plpy.prepare("SELECT convert_from(file, 'utf-8') FROM module_files AS mf NATURAL JOIN files AS f WHERE module_ident = $1 AND filename = 'index.cnxml.html'", ('integer',))
   html = plpy.execute(plan, (module_ident,))[0]['convert_from']
   content, warning_messages = transform_module_content(html, 'html2cnxml', plpy, module_ident)
