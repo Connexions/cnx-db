5186e2058b6cca04462a866dffb4ca52397f0916
Michael Mulich <michael.mulich@gmail.com>
2017-11-02 13:38:29 -0700

    Allow discover_settings to take an existing settings value
    This is used to allow an application to discover the settings, but also define them if they so choose.

--- cnxdb/config.py
+++ cnxdb/config.py
@@ -3,9 +3,10 @@ import os
 __all__ = ('discover_settings')
 
 
-def discover_settings():
+def discover_settings(settings=None):
     """Discover settings from environment variables
 
+    :param dict settings: An existing settings value
     :return: dictionary of settings
     :rtype: dict
 
@@ -21,8 +22,13 @@ def discover_settings():
     if common_url is None:
         raise RuntimeError("'DB_URL' environment variable must be defined")
 
-    settings = {
+    new_settings = {
         'db.common.url': common_url,
         'db.super.url': super_url,
     }
+
+    if settings is None:
+        settings = {}
+    for k, v in new_settings.items():
+        settings.setdefault(k, v)
     return settings
