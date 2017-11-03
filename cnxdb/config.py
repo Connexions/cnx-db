d2ba291a230d74e3da7f182d43246a17888f8689
Michael Mulich <michael.mulich@gmail.com>
2017-11-02 20:27:23 -0700

    Add an integration point for the pyramid webframework

--- cnxdb/config.py
+++ cnxdb/config.py
@@ -16,19 +16,21 @@ def discover_settings(settings=None):
        defaults and required settings.
 
     """
+    if settings is None:
+        settings = {}
+
     common_url = os.environ.get('DB_URL', None)
     super_url = os.environ.get('DB_SUPER_URL', common_url)
 
-    if common_url is None:
-        raise RuntimeError("'DB_URL' environment variable must be defined")
+    if common_url is None and settings.get('db.common.url') is None:
+        raise RuntimeError("'DB_URL' environment variable "
+                           "OR the 'db.common.url' setting MUST be defined")
 
     new_settings = {
         'db.common.url': common_url,
         'db.super.url': super_url,
     }
 
-    if settings is None:
-        settings = {}
     for k, v in new_settings.items():
         settings.setdefault(k, v)
     return settings
