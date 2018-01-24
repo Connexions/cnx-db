fc91efb2745f567d3828c80e839de4cfd0811c7a
Michael Mulich <michael.mulich@gmail.com>
2018-01-24 11:19:30 -0800

    Factor out the DB_NAME container environment variable
    The DB_NAME environment variable doesn't appear to be needed anymore since
    we are having the postgres container's entrypoint script create the database
    for us.

--- .dockerfiles/initdb.d/00_initdb.sh
+++ .dockerfiles/initdb.d/00_initdb.sh
@@ -4,10 +4,10 @@ set -e
 psql -v ON_ERROR_STOP=1 --username postgres <<-EOSQL
     CREATE USER ${DB_USER};
     CREATE USER backups;
-    ALTER DATABASE "${DB_NAME}" OWNER TO ${DB_USER};
-    GRANT ALL PRIVILEGES ON DATABASE "${DB_NAME}" TO ${POSTGRES_USER};
-    GRANT ALL PRIVILEGES ON DATABASE "${DB_NAME}" TO ${DB_USER};
-    \c "${DB_NAME}" ${POSTGRES_USER}
+    ALTER DATABASE "${POSTGRES_DB}" OWNER TO ${DB_USER};
+    GRANT ALL PRIVILEGES ON DATABASE "${POSTGRES_DB}" TO ${POSTGRES_USER};
+    GRANT ALL PRIVILEGES ON DATABASE "${POSTGRES_DB}" TO ${DB_USER};
+    \c "${POSTGRES_DB}" ${POSTGRES_USER}
     GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA PUBLIC TO ${POSTGRES_USER};
     GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA PUBLIC TO ${DB_USER};
 EOSQL
@@ -17,6 +17,6 @@ if [ -z "`ls /docker-entrypoint-initdb.d/*.sql.gz`" -a -z  "`ls /docker-entrypoi
 fi
 
 # ??? Is this really what we want to be doing?
-psql -v ON_ERROR_STOP=1 --username postgres "$DB_NAME" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username postgres "$POSTGRES_DB" <<-EOSQL
     REASSIGN OWNED BY ${POSTGRES_USER} TO ${DB_USER};
 EOSQL
