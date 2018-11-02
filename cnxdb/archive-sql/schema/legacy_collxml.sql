203ec944b0d561bf0efc25c338d6fc01dba27ac6
Ross Reedstrom <reedstrm@gmail.com>
2018-11-02 14:27:24 -0500

    correct collection.xml generation function (#166)
    * correct mdml generation function
    
    * fix child order in tree

--- cnxdb/archive-sql/schema/legacy_collxml.sql
+++ cnxdb/archive-sql/schema/legacy_collxml.sql
@@ -60,9 +60,9 @@ SELECT
       ),
 
       xmlelement(name "md:roles",
-            xmlelement(name "md:role", xmlattributes('authors' as "type"), array_to_string(m.authors,' ')),
-            xmlelement(name "md:role", xmlattributes('maintainers' as "type"), array_to_string(m.maintainers,' ')),
-            xmlelement(name "md:role", xmlattributes('licensors' as "type"), array_to_string(m.licensors,' ')),
+            xmlelement(name "md:role", xmlattributes('author' as "type"), array_to_string(m.authors,' ')),
+            xmlelement(name "md:role", xmlattributes('maintainer' as "type"), array_to_string(m.maintainers,' ')),
+            xmlelement(name "md:role", xmlattributes('licensor' as "type"), array_to_string(m.licensors,' ')),
             (SELECT xmlelement(name "md:role", xmlattributes(roleparam as "type"), array_to_string(personids, ' ')) FROM
                 roles r join moduleoptionalroles mor on r.roleid = mor.roleid WHERE mor.module_ident = m.module_ident
             )
@@ -179,7 +179,7 @@ SELECT
     WHERE t.nodeid = legacy_subcol.nodeid
 $$;
 
--- redefine or above, since needs to recurse back to subcol
+-- redefine for above, since needs to recurse back to subcol
 -- recursion terminates on all-module subcols
 
 CREATE OR REPLACE FUNCTION legacy_content(nodeid int,
@@ -192,13 +192,15 @@ $$
 SELECT
     xmlelement(name "col:content",
     (SELECT xmlagg(
-            CASE WHEN portal_type in ('Module','CompositeModule')
-                THEN legacy_module(t.nodeid, legacy_content.repo)
-                WHEN portal_type in ('SubCollection','CompositeSubCollection')
-                THEN legacy_subcol(t.nodeid, legacy_content.repo)
+            CASE WHEN c.portal_type in ('Module','CompositeModule')
+                THEN legacy_module(c.nodeid, legacy_content.repo)
+                WHEN c.portal_type in ('SubCollection','CompositeSubCollection')
+                THEN legacy_subcol(c.nodeid, legacy_content.repo)
             END)
-            FROM trees t join modules m on t.documentid = m.module_ident
+            FROM (SELECT nodeid, portal_type FROM trees t JOIN modules m
+                ON t.documentid = m.module_ident
                 WHERE parent_id = legacy_content.nodeid
+                ORDER BY childorder) AS c
             )
         )
 $$;
