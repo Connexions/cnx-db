13f9426ee65b340ab650e2c22718dad07e948f82
Ross Reedstrom <reedstrm@gmail.com>
2018-11-07 15:24:47 -0600

    add replace_collxml function

--- cnxdb/archive-sql/schema/legacy_collxml.sql
+++ cnxdb/archive-sql/schema/legacy_collxml.sql
@@ -240,3 +240,41 @@ SELECT xmlelement(name "col:collection",
 FROM modules m JOIN trees t ON m.module_ident = t.documentid WHERE m.module_ident = legacy_collxml.ident
 $$;
 
+CREATE OR REPLACE FUNCTION replace_collxml(coll_id integer)
+ RETURNS integer
+ LANGUAGE plpgsql
+AS $function$
+  DECLARE
+    existing_collxml_id integer;
+    _collxml_id integer;
+
+  BEGIN
+    existing_collxml_id := (SELECT fileid FROM module_files
+        WHERE module_ident = coll_id AND filename = 'collection.xml');
+
+    SELECT fileid FROM files WHERE sha1 = 
+      sha1(pretty_print(legacy_collxml(coll_id, True))::text::bytea)
+        INTO _collxml_id;
+
+    IF _collxml_id IS NULL THEN
+      INSERT INTO files (file, media_type) SELECT
+        pretty_print(legacy_collxml(coll_id, True))::text::bytea,
+        'text/xml'
+        RETURNING fileid INTO _collxml_id;
+    END IF;
+
+
+    IF existing_collxml_id IS NULL THEN
+      INSERT INTO module_files (module_ident, fileid, filename)
+        VALUES ( coll_id, _collxml_id, 'collection.xml');
+    ELSIF _collxml_id != existing_collxml_id THEN
+      DELETE FROM module_files WHERE module_ident = coll_id AND filename = 'collection.xml';
+      DELETE FROM files WHERE fileid = existing_collxml_id;
+      INSERT INTO module_files (module_ident, fileid, filename)
+        VALUES ( coll_id, _collxml_id, 'collection.xml');
+    END IF;
+
+    RETURN _collxml_id;
+  END;
+
+$function$
