7f2623f6de06538b2807f6c233f2237738a87091
Michael Mulich <michael.mulich@gmail.com>
2018-11-08 21:13:20 -0800

    Adjust the container POSTGRES_USER value
    Upstream changes have made it so the `postgres` role is no longer created
    along with the `$POSTGRES_USER`, which defaults to `postgres`.
    Because of this, we'll stick with the default and create our superuser
    in the init script.
    
    These changes should keep things the same, just reorder them slightly.

--- .dockerfiles/initdb.d/00_initdb.sh
+++ .dockerfiles/initdb.d/00_initdb.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username postgres <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username ${POSTGRES_USER} <<-EOSQL
+    CREATE USER rhaptos_admin SUPERUSER;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username rhaptos_admin -d postgres <<-EOSQL
     CREATE USER ${DB_USER};
     CREATE USER backups;
     ALTER DATABASE "${POSTGRES_DB}" OWNER TO ${DB_USER};
-    GRANT ALL PRIVILEGES ON DATABASE "${POSTGRES_DB}" TO ${POSTGRES_USER};
     GRANT ALL PRIVILEGES ON DATABASE "${POSTGRES_DB}" TO ${DB_USER};
-    \c "${POSTGRES_DB}" ${POSTGRES_USER}
-    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA PUBLIC TO ${POSTGRES_USER};
+    \c "${POSTGRES_DB}" rhaptos_admin
     GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA PUBLIC TO ${DB_USER};
 EOSQL
+
 
 if [ -z "`ls /docker-entrypoint-initdb.d/*.sql.gz`" -a -z  "`ls /docker-entrypoint-initdb.d/*.sql`" ]; then
     cnx-db init
 fi
 
 # ??? Is this really what we want to be doing?
-psql -v ON_ERROR_STOP=1 --username postgres "$POSTGRES_DB" <<-EOSQL
-    REASSIGN OWNED BY ${POSTGRES_USER} TO ${DB_USER};
+psql -v ON_ERROR_STOP=1 --username ${POSTGRES_USER} "$POSTGRES_DB" <<-EOSQL
+    REASSIGN OWNED BY rhaptos_admin TO ${DB_USER};
 EOSQL
