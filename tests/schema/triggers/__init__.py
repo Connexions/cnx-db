cd19b67d3a2983b7e3446a0d18e6cb2830536a3e
Michael Mulich <michael.mulich@gmail.com>
2019-04-11 16:39:18 -0700

    Split the triggers tests into separate modules

---
+++ tests/schema/triggers/__init__.py
@@ -0,0 +1,5 @@
+# Note, the triggers are only python 2.x compatible. It's assumed,
+# at least for now, that in-database logic (i.e. triggers) are only
+# run within a python2 environment. This product is to be setup in a
+# production environment running within the database under python2 and
+# optionally running within application code under either python2 or python3.
